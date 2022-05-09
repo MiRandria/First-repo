20d80dd12bae4612746bb6515139666505a37efc
MiRandria <103509226+MiRandria@users.noreply.github.com>
2022-05-09 21:03:05 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,58 @@
+const pop= document.querySelector(".pop")
+const icon= document.querySelector(".realisation .icon")
+const realisation= document.querySelector(".realisation")
+
+
+pop.addEventListener("click", function(){
+    realisation.classList.toggle("show")
+
+    
+})
+icon.addEventListener("click", function(){
+    realisation.classList.toggle("show")
+    
+})
+const pop1= document.querySelector(".pop1")
+const icon1= document.querySelector(".realisation1 .icon1")
+const realisation1= document.querySelector(".realisation1")
+
+
+pop1.addEventListener("click", function(){
+    realisation1.classList.toggle("show1")
+
+    
+})
+icon1.addEventListener("click", function(){
+    realisation1.classList.toggle("show1")
+    
+})
+const pop2= document.querySelector(".pop2")
+const icon2= document.querySelector(".realisation2 .icon2")
+const realisation2= document.querySelector(".realisation2")
+
+
+pop2.addEventListener("click", function(){
+    realisation2.classList.toggle("show2")
+
+    
+})
+icon2.addEventListener("click", function(){
+    realisation2.classList.toggle("show2")
+    
+})
+const pop3= document.querySelector(".pop3")
+const icon3= document.querySelector(".realisation3 .icon3")
+const realisation3= document.querySelector(".realisation3")
+
+
+pop3.addEventListener("click", function(){
+    realisation3.classList.toggle("show3")
+
+    
+})
+icon3.addEventListener("click", function(){
+    realisation3.classList.toggle("show3")
+    
+})
+
+
